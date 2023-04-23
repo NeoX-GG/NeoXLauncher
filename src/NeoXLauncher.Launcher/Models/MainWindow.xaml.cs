75f9e8997aeb9a0c3739f3e2d0632d37f0218bcc
ZroFreaks <42916282+ZroIsHere@users.noreply.github.com>
2023-04-24 00:13:44 +0200

    Redesign

--- src/NeoXLauncher.Launcher/Models/MainWindow.xaml.cs
+++ src/NeoXLauncher.Launcher/Models/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using NeoXLauncher.Launcher.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace NeoXLauncher.Launcher.Models;
 
@@ -20,48 +9,9 @@ namespace NeoXLauncher.Launcher.Models;
 /// </summary>
 public partial class MainWindow : Window
 {
-    MainSubWindowType SubWindowType;
-
     public MainWindow()
     {
         InitializeComponent();
-        //HomeButton_MouseLeftButtonDown(null, null);
-    }
-
-    private void HomeButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-    {
-#if DEBUG
-        MessageBox.Show("In development");
-        return;
-#endif
-        MessageBox.Show("In development");
-    }
-
-    private void NewsButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-    {
-#if DEBUG
-        MessageBox.Show("In development");
-        return;
-#endif
-        MessageBox.Show("In development");
-    }
-
-    private void GiftButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-    {
-#if DEBUG
-        MessageBox.Show("In development");
-        return;
-#endif
-        MessageBox.Show("In development");
-    }
-
-    private void ConfigButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-    {
-#if DEBUG
-        MessageBox.Show("In development");
-        return;
-#endif
-        MessageBox.Show("In development");
     }
 
     private void CloseButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -69,45 +19,19 @@ public partial class MainWindow : Window
         Environment.Exit(1);
     }
 
-    void ChangeButtonHover(MainSubWindowType NewType)
+    private void HomeButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
     {
-        if (SubWindowType == NewType)
-        {
-            return;
-        }
+        LibraryStackPanel.Visibility = Visibility.Collapsed;
+        LibraryButton.Opacity = 0.8;
+        HomeStackPanel.Visibility = Visibility.Visible;
+        HomeButton.Opacity = 1;
+    }
 
-        switch(SubWindowType)
-        {
-            case MainSubWindowType.Home:
-                HomeButton.Source = new BitmapImage(new Uri(@"../Images/Home.png", UriKind.Relative));
-                break;
-            case MainSubWindowType.News:
-                NewsButton.Source = new BitmapImage(new Uri(@"../Images/News.png", UriKind.Relative));
-                break;
-            case MainSubWindowType.Gift:
-                GiftButton.Source = new BitmapImage(new Uri(@"../Images/Gift.png", UriKind.Relative));
-                break;
-            case MainSubWindowType.Config:
-                ConfigButton.Source = new BitmapImage(new Uri(@"../Images/Config.png", UriKind.Relative));
-                break;
-        }
-
-        SubWindowType = NewType;
-
-        switch (SubWindowType)
-        {
-            case MainSubWindowType.Home:
-                HomeButton.Source = new BitmapImage(new Uri(@"../Images/HomeHover.png", UriKind.Relative));
-                break;
-            case MainSubWindowType.News:
-                NewsButton.Source = new BitmapImage(new Uri(@"../Images/NewsHover.png", UriKind.Relative));
-                break;
-            case MainSubWindowType.Gift:
-                GiftButton.Source = new BitmapImage(new Uri(@"../Images/GiftHover.png", UriKind.Relative));
-                break;
-            case MainSubWindowType.Config:
-                ConfigButton.Source = new BitmapImage(new Uri(@"../Images/ConfigHover.png", UriKind.Relative));
-                break;
-        }
+    private void LibraryButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        HomeStackPanel.Visibility = Visibility.Collapsed;
+        HomeButton.Opacity = 0.8;
+        LibraryStackPanel.Visibility = Visibility.Visible;
+        LibraryButton.Opacity = 1;
     }
 }
